8cf41c8ad1f2eaa8b4780b00cf41b46d0178f4a3
bmk5 <briankamau167@gmail.com>
2025-11-26 11:44:02 +0300

    added frontend and backend folders

---
+++ frontend/.next/dev/static/chunks/app_(dashboard)_layout_tsx_483c3b78._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c44ea609._.js",
+  "static/chunks/node_modules__pnpm_e2cd8fcc._.js"
+],
+    source: "dynamic"
+});
---
+++ frontend/.next/dev/static/chunks/app_(dashboard)_page_tsx_cbdfa838._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_49e8b714._.js"
+],
+    source: "dynamic"
+});
